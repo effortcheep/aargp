562ecf545ea1d8548121e23ceba13359fa496512
effort <effortcheep@163.com>
2026-03-16 21:55:17 +0800

    feat: 敌人受击动画 销毁动画

--- Enemise/Scripts/enemy.gd
+++ Enemise/Scripts/enemy.gd
@@ -2,6 +2,7 @@ class_name Enemy extends CharacterBody2D
 
 signal direction_change(new_direction: Vector2)
 signal enemy_damaged()
+signal enemy_destroyed()
 
 const DIR_4 = [Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP]
 
@@ -14,6 +15,7 @@ var invulneralbe: bool = false
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var hit_box: HitBox = $HitBox
 @onready var state_machine: EnemyStateMachine = $EnemyStateMachine
 
 
@@ -21,6 +23,7 @@ var invulneralbe: bool = false
 func _ready():
 	state_machine.initialize(self)
 	player = PlayerManager.player
+	hit_box.Damaged.connect(_take_damage)
 	pass # Replace with function body.
 
 
@@ -60,3 +63,13 @@ func AnimDirection() -> String:
 		return 'up'
 	else:
 		return "side"
+
+func _take_damage(damage: int) -> void:
+	if invulneralbe == true:
+		return
+	hp -= damage
+	if hp > 0:
+		enemy_damaged.emit()
+	else:
+		enemy_destroyed.emit()
+		
